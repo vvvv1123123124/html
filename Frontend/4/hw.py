7e65806f44df126a8d144a7f7de3a89d57d31c75
vvvv1123123124 <vadi5656kx@gmail.com>
2025-01-30 19:22:39 +0300

    Add files via upload

---
+++ Frontend/4/hw.py
@@ -0,0 +1,11 @@
+a = int(input('Введите первое число:'))
+b = int(input('Введите второе число:'))
+c = int(input('Введите третье число:'))
+
+addition = a + b + c
+product = a * b * c
+average = addition/3
+
+print(a,'+',b,'+',c,'=',addition)
+print(a,'*',b,'*',c,'=',product)
+print('Среднее арифметическое чисел ',a,',',b,'и',c,'это ',average)
